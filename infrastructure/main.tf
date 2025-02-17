65b694343f79716f235ca568979fbf18fe9adfb0
gom1be <marco.gora@bosch.com>
2025-02-17 13:59:20 +0100

    fix: properly implement vnet integration and connect to storage account via managed id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,6 +23,20 @@ resource "azurerm_subnet" "backend" {
   resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
+  delegation {
+    name = "Microsoft.Web/serverFarms"
+    service_delegation {
+      actions = [ "Microsoft.Network/virtualNetworks/subnets/action" ]
+      name    = "Microsoft.Web/serverFarms"
+    }
+  }
+}
+
+resource "azurerm_subnet" "storage" {
+  name                 = "${var.app_name}-storage-subnet"
+  resource_group_name  = azurerm_resource_group.app.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 ############
@@ -56,19 +70,25 @@ resource "cloudflare_dns_record" "frontend" {
 ###########
 
 resource "azurerm_storage_account" "backend" {
-  name                     = "${var.app_name}storage"
-  resource_group_name      = azurerm_resource_group.app.name
-  location                 = azurerm_resource_group.app.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                      = "${var.app_name}storage"
+  resource_group_name       = azurerm_resource_group.app.name
+  location                  = azurerm_resource_group.app.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  shared_access_key_enabled = false
 }
 
 resource "azurerm_private_endpoint" "storage" {
   name                          = "${var.app_name}-storage-pe"
   resource_group_name           = azurerm_resource_group.app.name
   location                      = azurerm_resource_group.app.location
-  subnet_id                     = azurerm_subnet.backend.id
+  subnet_id                     = azurerm_subnet.storage.id
   custom_network_interface_name = "${var.app_name}-storage-nic"
+
+  private_dns_zone_group {
+    name = "backend-storage"
+    private_dns_zone_ids = [resource.azurerm_private_dns_zone.storage.id]
+  }
 
   private_service_connection {
     name                           = "storage"
@@ -82,8 +102,13 @@ resource "azurerm_private_endpoint" "table" {
   name                          = "${var.app_name}-table-pe"
   resource_group_name           = azurerm_resource_group.app.name
   location                      = azurerm_resource_group.app.location
-  subnet_id                     = azurerm_subnet.backend.id
+  subnet_id                     = azurerm_subnet.storage.id
   custom_network_interface_name = "${var.app_name}-table-nic"
+
+  private_dns_zone_group {
+    name = "backend-storage"
+    private_dns_zone_ids = [resource.azurerm_private_dns_zone.storage.id]
+  }
 
   private_service_connection {
     name                           = "storage"
@@ -126,16 +151,35 @@ resource "azurerm_service_plan" "backend_plan" {
 }
 
 resource "azurerm_linux_function_app" "backend" {
-  name                 = "${var.app_name}-backend"
-  resource_group_name  = azurerm_resource_group.app.name
-  location             = azurerm_resource_group.app.location
-  service_plan_id      = azurerm_service_plan.backend_plan.id
-  storage_account_name = azurerm_storage_account.backend.name
+  name                          = "${var.app_name}-backend"
+  resource_group_name           = azurerm_resource_group.app.name
+  location                      = azurerm_resource_group.app.location
+  service_plan_id               = azurerm_service_plan.backend_plan.id
+  storage_account_name          = azurerm_storage_account.backend.name
+  https_only                    = true
+  storage_uses_managed_identity = true
+  virtual_network_subnet_id     = azurerm_subnet.backend.id
+
+  identity {
+    type = "SystemAssigned"
+  }
   
   site_config {
+    minimum_tls_version = "1.3"
+
     cors {
       allowed_origins     = ["${var.domain_name}", "${azurerm_static_web_app.frontend.default_host_name}"]
       support_credentials = false
     }
+
+    application_stack {
+      node_version = "20"
+    }
   }
+}
+
+resource "azurerm_role_assignment" "function_storage" {
+  scope                = azurerm_storage_account.backend.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_linux_function_app.backend.identity[0].principal_id
 }
